6cce865044aaa6f40ff046105627fb2b99a11744
mobilemelody <melody.reebs@gmail.com>
2020-06-24 16:01:59 -0700

    Initial setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const pug = require('pug');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.send('Hello, world!');
+});
+
+app.listen(PORT, function(){
+  console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.');
+});
